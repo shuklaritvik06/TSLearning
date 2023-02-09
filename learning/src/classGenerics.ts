919ff789d9e43e31f6bbe5e322d0cbd16c795582
Ritvik Shukla <ritvikshukla261@gmail.com>
2023-02-09 21:24:43 +0530

    init

---
+++ learning/src/classGenerics.ts
@@ -0,0 +1,15 @@
+class DataStorage<T> {
+  public data: T[] = [];
+  addItem(item: T) {
+    this.data.push(item);
+  }
+  removeItem(item: T) {
+    this.data.splice(this.data.indexOf(item), 1);
+  }
+  getItems() {
+    return [...this.data];
+  }
+}
+
+const bgc = new DataStorage<string>();
+console.log(bgc.data);
--- learning/src/generics.ts
+++ learning/src/generics.ts
@@ -1,19 +1,38 @@
-const myFunction = <T extends object>(obj: T)=>{
-    return obj;
-}
+const myFunction = <T extends object>(obj: T) => {
+  return obj;
+};
 
-myFunction({name: 'John'});
+myFunction({ name: "John" });
 
-interface Human<T>{
-    name: string,
-    age: number,
-    salary: number,
-    kids: T
+interface Human<T> {
+  name: string;
+  age: number;
+  salary: number;
+  kids: T;
 }
 
 const humanA: Human<number> = {
-    name: 'John',
-    age: 20,
-    salary: Infinity,
-    kids: 2
+  name: "John",
+  age: 20,
+  salary: Infinity,
+  kids: 2
+};
+
+function merge<T extends object, U>(obj: T, obj2: U) {
+  return Object.assign(obj, obj2);
+}
+
+const cd = merge({ name: "rames" }, { age: 23 });
+console.log(cd.age);
+
+function hell<U extends { name: string }>(obj: U): U {
+  console.log(obj.name);
+  return obj;
+}
+
+function extractFromObject<T extends object, U extends keyof T>(
+  obj: T,
+  key: U
+) {
+  return obj[key];
 }
